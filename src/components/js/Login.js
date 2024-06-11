4522cbd9127784c375f949a5e9d9ee0c79078741
KULDEEP YADAV <ky2903@gmail.com>
2024-06-11 12:28:40 +0530

    login error

--- src/components/js/Login.js
+++ src/components/js/Login.js
@@ -1,118 +1,4 @@
-// import React, { useState } from "react";
-// import '../css/Login.css';
-// import right_arrow from '../Assets/next.png';
-// import loading from '../Assets/loading.gif';
-// import favicon from '../Assets/white_logo.png';
-// import { Button } from "react-bootstrap";
-// import UserServices from "../../services/UserServices";
-// import { ToastContainer, toast } from "react-toastify";
-// import { useNavigate } from "react-router-dom";
-
-// function Login() {
-
-//     const navigate = useNavigate();
-
-//     const navOtp = () => {
-//         navigate('/otp', { state: { email: email } });
-//     }
-
-//     const [arrow, setArrow] = useState(right_arrow);
-//     const [userStyle, setUserStyle] = useState("web_ncyb_login_flx_input");
-//     const [userLock, setUserLock] = useState(false);
-//     const [passwordBool, setPasswordBool] = useState(false);
-    
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState("");
-
-//     const blurEmail = e => {
-//         setEmail(e.target.value);
-//     };
-
-//     const blurPassword = e => {
-//         setPassword(e.target.value);
-//     };
-
-//     const checkUser = async () => {
-//         if(email.length>0)
-//         {
-//             UserServices.checkUserID(email)
-//             .then(response => {
-//                 if (response.data.status === true) {
-//                     userValidated();
-//                     toast.success("EMAIL ID VERIFIED!");
-//                 }
-//                 else {
-//                     toast.error("INVALID EMAIL ID!");
-//                 }
-//             })
-//             .catch(e => {
-//                 console.log(e);
-//             });
-//         }
-//         else{
-//             toast.error("INVALID EMAIL ID!");
-//         }
-//     };
-
-//     const checkPassword = async () => {
-//         if(password.length>0)
-//         {
-//             const dat = {
-//                 email: email,
-//                 password: password
-//             };
-//             UserServices.checkPass(dat)
-//             .then(response => {
-//                 if (response.data.status === true) {
-//                     userValidated();
-//                     toast.success("USER VERIFIED!");
-//                     navOtp();
-//                 }
-//                 else {
-//                     toast.error("INVALID PASSWORD!");
-//                 }
-//             })
-//             .catch(e => {
-//                 console.log(e);
-//             });
-//         }
-//         else{
-//             toast.error("INVALID PASSWORD!");
-//         }
-//     };
-
-//     const userValidated = async() =>{
-//         await setArrow(loading);
-//         await setUserStyle("web_ncyb_login_flx_input_top");
-//         await setUserLock(true);
-//         await setPasswordBool(true);
-//         await setArrow(null);
-//     } 
-
-//     return (
-//         <div>
-//              <ToastContainer position="top-right" />
-//             <div className="web_ncyb_login">
-//                 <div className="web_ncyb_login_flx">
-//                     <div >
-//                         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: '30px' }}><img alt="favicon" src={favicon} height={45} style={{ borderRadius: '15px' }}></img><h3 className="web_ncyb_login_flx_inner_heading">Sign In to CasualtyCat</h3></div>
-//                         <div className="web_ncyb_login_flx_inner">
-//                             <div style={{ position: 'relative' }}><input disabled={userLock} placeholder="Username" className={userStyle} onBlur={blurEmail} ></input><img alt="icon" className="icon" width={45} style={{ paddingRight: '10px' }} src={arrow} onClick={() => checkUser()}></img></div>
-//                             {passwordBool && <div style={{ height: '0.6px', backgroundColor: '#e1e2e3' }}></div>}
-//                             {passwordBool && <div style={{ position: 'relative' }}><input type="password" placeholder="Password" className="web_ncyb_login_flx_input_bottom" onBlur={blurPassword}></input><img alt="icon" className="icon" width={45} style={{ paddingRight: '10px' }} src={right_arrow} onClick={() => checkPassword()}></img></div>}
-//                         </div>
-//                         <div style={{ display: 'grid', placeItems: 'center', marginTop: '30px', fontFamily: 'Poppins' }}><Button style={{ padding: '0px', margin: '0px', fontSize: '17px', color: '#fff' }} variant="link">Forgot Password ➚</Button></div>
-//                         <a href="/superAdminDashboard">Home page</a>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-// export default Login;
-
-
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import '../css/Login.css';
 import right_arrow from '../Assets/next.png';
 import loading from '../Assets/loading.gif';
@@ -123,6 +9,7 @@ import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
+
     const navigate = useNavigate();
 
     const navOtp = () => {
@@ -133,14 +20,9 @@ function Login() {
     const [userStyle, setUserStyle] = useState("web_ncyb_login_flx_input");
     const [userLock, setUserLock] = useState(false);
     const [passwordBool, setPasswordBool] = useState(false);
+    
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
-    // Set default username and password on component mount
-    useEffect(() => {
-        setEmail("admin");
-        setPassword("1234");
-    }, []);
 
     const blurEmail = e => {
         setEmail(e.target.value);
@@ -215,9 +97,9 @@ function Login() {
                     <div >
                         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: '30px' }}><img alt="favicon" src={favicon} height={45} style={{ borderRadius: '15px' }}></img><h3 className="web_ncyb_login_flx_inner_heading">Sign In to CasualtyCat</h3></div>
                         <div className="web_ncyb_login_flx_inner">
-                            <div style={{ position: 'relative' }}><input disabled={userLock} placeholder="Username" className={userStyle} value={email} onBlur={blurEmail} ></input><img alt="icon" className="icon" width={45} style={{ paddingRight: '10px' }} src={arrow} onClick={() => checkUser()}></img></div>
+                            <div style={{ position: 'relative' }}><input disabled={userLock} placeholder="Username" className={userStyle} onBlur={blurEmail} ></input><img alt="icon" className="icon" width={45} style={{ paddingRight: '10px' }} src={arrow} onClick={() => checkUser()}></img></div>
                             {passwordBool && <div style={{ height: '0.6px', backgroundColor: '#e1e2e3' }}></div>}
-                            {passwordBool && <div style={{ position: 'relative' }}><input type="password" placeholder="Password" className="web_ncyb_login_flx_input_bottom" value={password} onBlur={blurPassword}></input><img alt="icon" className="icon" width={45} style={{ paddingRight: '10px' }} src={right_arrow} onClick={() => checkPassword()}></img></div>}
+                            {passwordBool && <div style={{ position: 'relative' }}><input type="password" placeholder="Password" className="web_ncyb_login_flx_input_bottom" onBlur={blurPassword}></input><img alt="icon" className="icon" width={45} style={{ paddingRight: '10px' }} src={right_arrow} onClick={() => checkPassword()}></img></div>}
                         </div>
                         <div style={{ display: 'grid', placeItems: 'center', marginTop: '30px', fontFamily: 'Poppins' }}><Button style={{ padding: '0px', margin: '0px', fontSize: '17px', color: '#fff' }} variant="link">Forgot Password ➚</Button></div>
                         <a href="/superAdminDashboard">Home page</a>
@@ -228,3 +110,4 @@ function Login() {
     );
 }
 export default Login;
+
